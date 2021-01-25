eab1d2d46f19fb3c28b74903d61931a43e99976d
MikTar <63552280+MikTar@users.noreply.github.com>
2021-01-25 22:14:04 +0100

    Delete php directory

--- php/send.php
+++
@@ -1,8 +0,0 @@
-$name =  $_POST['name'];
-$contact = $_POST['contact'];
-$comment = $_POST['comment'];
-$my-email = "mikael.tar@outlook.com"
-$backurl = "../index.html";
-
-mail($my-email, $name, $contact, $comment )
-
