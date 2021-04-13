19a2f2a55cab641665fee30545ff6d9cf4cd1ff2
MikTar <courcillon@gmail.com>
2021-04-13 13:02:42 +0200

    function change

--- letter-count/scripts/main.js
+++ letter-count/scripts/main.js
@@ -1,12 +1,15 @@
 function myFunction(){
     
     let textValue = document.getElementById('textareaID').value;
+    textValue = textValue.replace(/\s+/g, ' ');
+    textValue = textValue.trim();
+    let numSpaces = textValue.split(" ").length;
     let textValueNoSpaces = textValue.replace(/[\s1234567890.,%:"'!`#$^&*()_+|',-/.../'?;@ АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщьэюя]/g, '');
     let textLength = textValueNoSpaces.length;
     let p = document.getElementById("result");
     let textResult = document.querySelector('#text-result');
     let h3 = document.querySelector('h3');
-    p.innerText = ` Количество символов : ${textLength}`;
+    p.innerText = ` Количество символов : ${textLength}.Количество слов : ${numSpaces}`;
     textResult.innerText = textValueNoSpaces;
     h3.innerText = 'Полученный текст(без пробелов, знаков препинания и кирических букв):'
     h3.style.padding = '1rem 2rem'
