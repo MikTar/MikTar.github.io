00f25440beb360017e625c886b3f73fab561651b
MikTar <courcillon@gmail.com>
2021-05-01 19:06:11 +0200

    added seo

--- auto-repair-shop-website/scripts/main.js
+++ auto-repair-shop-website/scripts/main.js
@@ -1,31 +1,30 @@
-
 //DATE
 
 let today = new Date();
-let dd =today.getDate();
-let mm =today.getMonth();
+let dd = today.getDate();
+let mm = today.getMonth();
 let dayOfWeek = today.getDay();
 
 //--------------DAY TRADUCTION--/
-if(dayOfWeek ===0){
+if (dayOfWeek === 0) {
   dayOfWeek = 'Вторник'
 }
-if(dayOfWeek ===1){
+if (dayOfWeek === 1) {
   dayOfWeek = 'Среда'
 }
-if(dayOfWeek ===2){
+if (dayOfWeek === 2) {
   dayOfWeek = 'Четверг'
 }
-if(dayOfWeek ===3){
+if (dayOfWeek === 3) {
   dayOfWeek = 'Пятница'
 }
-if(dayOfWeek ===4){
+if (dayOfWeek === 4) {
   dayOfWeek = 'Суббота'
 }
-if(dayOfWeek ===5){
+if (dayOfWeek === 5) {
   dayOfWeek = 'Воскресенье'
 }
-if(dayOfWeek ===6){
+if (dayOfWeek === 6) {
   dayOfWeek = 'Понедельник'
 }
 
@@ -33,8 +32,8 @@ if(dayOfWeek ===6){
 mm = mm + 1;
 mm = String(mm)
 
-if(mm.length < 2){
-    mm = `0${mm}`
+if (mm.length < 2) {
+  mm = `0${mm}`
 }
 
 let divToday = document.querySelector('#date-today');
@@ -56,31 +55,31 @@ let firstDate = document.querySelector('.first-date');
 let secondDate = document.querySelector('.second-date');
 let thirdDate = document.querySelector('.third-date');
 
-firstDate.addEventListener('click', function(){
-    firstDate.style.backgroundColor = 'orangered';
-    firstDate.style.color = 'white';
-    secondDate.style.backgroundColor = 'transparent';
-    thirdDate.style.backgroundColor = 'transparent';
-    secondDate.style.color = '#9497a1';
-    thirdDate.style.color = '#9497a1';
+firstDate.addEventListener('click', function () {
+  firstDate.style.backgroundColor = 'orangered';
+  firstDate.style.color = 'white';
+  secondDate.style.backgroundColor = 'transparent';
+  thirdDate.style.backgroundColor = 'transparent';
+  secondDate.style.color = '#9497a1';
+  thirdDate.style.color = '#9497a1';
 
 });
-secondDate.addEventListener('click', function(){
-    secondDate.style.backgroundColor = 'orangered';
-    secondDate.style.color = 'white';
-    thirdDate.style.backgroundColor = 'transparent';
-    firstDate.style.backgroundColor = 'transparent';
-    thirdDate.style.color = '#9497a1';
-    firstdDate.style.color = '#9497a1';
-    
+secondDate.addEventListener('click', function () {
+  secondDate.style.backgroundColor = 'orangered';
+  secondDate.style.color = 'white';
+  thirdDate.style.backgroundColor = 'transparent';
+  firstDate.style.backgroundColor = 'transparent';
+  thirdDate.style.color = '#9497a1';
+  firstdDate.style.color = '#9497a1';
+
 });
-thirdDate.addEventListener('click', function(){
-    thirdDate.style.backgroundColor = 'orangered';
-    thirdDate.style.color = 'white';
-    secondDate.style.backgroundColor = 'transparent';
-    firstDate.style.backgroundColor = 'transparent';
-    secondDate.style.color = '#9497a1';
-    firstDate.style.color = '#9497a1';
+thirdDate.addEventListener('click', function () {
+  thirdDate.style.backgroundColor = 'orangered';
+  thirdDate.style.color = 'white';
+  secondDate.style.backgroundColor = 'transparent';
+  firstDate.style.backgroundColor = 'transparent';
+  secondDate.style.color = '#9497a1';
+  firstDate.style.color = '#9497a1';
 });
 
 
@@ -93,9 +92,9 @@ thirdDate.addEventListener('click', function(){
 let divHours = document.querySelector('#div-hours');
 let selectedHour;
 
-divHours.onclick = function(event) {
+divHours.onclick = function (event) {
   let target = event.target; // где был клик?
-  if (target.tagName != 'SPAN')return; // не на TD? тогда не интересует
+  if (target.tagName != 'SPAN') return; // не на TD? тогда не интересует
 
   changeBgColor(target); // подсветить TD
 };
@@ -108,4 +107,3 @@ function changeBgColor(span) {
   selectedHour.classList.add('selected-hour'); // подсветить новый td
 }
 //-----------------------------------------------------------------------------------------------------------------//
-
